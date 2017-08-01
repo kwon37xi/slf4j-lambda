679c7ea7cd9cc817b8a6dab650a98f7d8c268e15
KwonNam Son <kwon37xi@gmail.com>
2017-08-02 00:23:59 +0900

    lazy test

--- slf4j-lambda-integration-test/src/main/java/kr/pe/kwonnam/slf4jlambda/integrationtest/Slf4jLambdaIntegrationMain.java
+++ slf4j-lambda-integration-test/src/main/java/kr/pe/kwonnam/slf4jlambda/integrationtest/Slf4jLambdaIntegrationMain.java
@@ -3,6 +3,8 @@ package kr.pe.kwonnam.slf4jlambda.integrationtest;
 import kr.pe.kwonnam.slf4jlambda.LambdaLogger;
 import kr.pe.kwonnam.slf4jlambda.LambdaLoggerFactory;
 
+import java.time.LocalDateTime;
+
 public class Slf4jLambdaIntegrationMain {
 
     public static void main(String[] args) {
@@ -10,9 +12,12 @@ public class Slf4jLambdaIntegrationMain {
 
         log.trace(() -> "trace message does not show. it's info level.");
         log.debug(() -> "debug message does not show. it's info level.");
+        log.debug(Slf4jLambdaIntegrationMain::createMessage); // does not show,
+
         log.info(() -> "info message");
         log.warn(() -> "warn message");
         log.error(() -> "error message");
+        log.info(Slf4jLambdaIntegrationMain::createMessage);
 
         log.info("lambda formatter number {}, string {}", () -> 123, () -> "Hello LambdaLogger");
         log.info("basic formatter number {}, string {}", 123, "Hello LambdaLogger");
@@ -23,6 +28,12 @@ public class Slf4jLambdaIntegrationMain {
             log.error(() -> "lambda exception msg - " + ex.getMessage(), ex);
             log.error("basic exception msg - " + ex.getMessage(), ex);
         }
+    }
+
+    public static String createMessage() {
+        String message = "message created - " + LocalDateTime.now();
+        System.out.println("### message created - " + message + " ###");
+        return message;
     }
 
     public static void createEx(String msg) {
